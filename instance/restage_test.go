0dd9fede32dba71accba67d958d93e6c56ac7912
Glyn Normington <gnormington@pivotal.io>
2017-10-06 14:15:19 +0100

    Improve usability of command tests
    Replace DescribeCommandTest by CommandTestBody which omits the outer
    `Describe`. The outer `Describe` may then be focussed, pended, etc. as usual.
    
    [#151480636]

--- instance/restage_test.go
+++ instance/restage_test.go
@@ -1,13 +1,14 @@
 package instance_test
 
 import (
+	. "github.com/onsi/ginkgo"
 	"github.com/pivotal-cf/spring-cloud-services-cli-plugin/httpclient/httpclientfakes"
 	"github.com/pivotal-cf/spring-cloud-services-cli-plugin/instance"
 )
 
-var _ = DescribeCommandTest("Restage", "restage",
+var _ = Describe("Restage", CommandTestBody("restage",
 	func(fakeAuthClient *httpclientfakes.FakeAuthenticatedClient, serviceInstanceAdminURL string,
 		accessToken string) (string, error) {
 
 		return instance.Restage(fakeAuthClient, serviceInstanceAdminURL, accessToken)
-	})
+	}))
